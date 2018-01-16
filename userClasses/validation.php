f1e6a5d607067259d5890c059fb1aacb9aa7afa3
Pritam Sangani <pritam.naran@yahoo.co.uk>
2018-01-16 14:56:29 +0000

    started created validation tests

---
+++ userClasses/validation.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * acts as a validator to validate all fields of bakers, customers and users being entered into the database.
+     */
+    class Validation
+    {
+
+        function __construct(argument)
+        {
+            # code...
+        }
+    }
+
+ ?>
