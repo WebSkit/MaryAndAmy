47f6fc59a90d8d93df380b4004d4ab7ba3b6c67d
Pritam Sangani <pritam.naran@yahoo.co.uk>
2018-01-17 13:52:10 +0000

    updated validation methods to validate email, postcode

--- userClasses/validation.php
+++ userClasses/validation.php
@@ -6,11 +6,39 @@
     {
         function validateEmail($email)
         {
-            $pattern = '';
-            preg_match($pattern, $email, $matches);
-
-            if($matches -> true)
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
-    }
+
+        function validateAddressLine1($addressLine1)
+        {
+
+        }
+
+        function validateAddressLine2($addressLine2)
+        {
+
+        }
+
+        function validatePostcode($postcode)
+        {
+            $pattern = '^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$'
+
+            if(preg_match($pattern, $postcode) -> true) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        function validateCountry($country)
+        {
+
+        }
 
  ?>
