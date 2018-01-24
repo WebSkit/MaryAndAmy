c072e42341524d1a2de82d75fdb34a2a64823656
Ben123456789101112 <35489655+Ben123456789101112@users.noreply.github.com>
2018-01-24 12:59:34 +0000

    Add files via upload
    I have replaced the old shop finder class with a new one, which in addition to the data it already returns, also returns an average review score and the distance between the user and the client(this is needed by the clients specification)

--- DAO/shopFinder.php
+++ DAO/shopFinder.php
@@ -37,19 +37,38 @@
 				
 				$distance=substr($arrayDistance["rows"][0]["elements"][0]["distance"]["text"],0,-3);//NOTE:gets the string, minus the last 3 characters(the space and the letters "mi"
 				//"[row][0][elements][0][distance][text]"=the location of the distance(miles) in the array
-			
+				//distance between the customer and the baker
 				if($distance<=$userAcceptedDistance && $distance<=$row["servedArea"])
 				{
-					$validCompaniesArray[$tempCount]=array("id"=>$row["bakerId"],"companyName"=>$row["companyName"],"addressLine1"=>$row["addressLine1"],"addressLine2"=>$row["addressLine2"],"postCode"=>$row["postCode"]);
+					$query="SELECT AVG(rating) AS average FROM product INNER JOIN review ON product.productId WHERE bakerId=".$row["bakerId"];
+
+					$resultReview=$connection->query($query);//finding the average review score
+					$reviewAverage="No Reviews";//default value if a baker has no reviews
+					echo $resultReview->num_rows;
+					
+					$rowReview=$resultReview->fetch_assoc();
+					
+					echo "the review score =".$rowReview["average"]."<br>";
+					if($rowReview["average"]==null)
+					{
+						echo "null entered";
+						$reviewAverage="No Reviews";
+						echo "reviewAverage value=".$reviewAverage;
+					}
+					else
+					{
+						echo "its not null entered";
+						$reviewAverage=$rowReview["average"];
+					}
+					$validCompaniesArray[$tempCount]=array("id"=>$row["bakerId"],"companyName"=>$row["companyName"],"addressLine1"=>$row["addressLine1"],"addressLine2"=>$row["addressLine2"],"postCode"=>$row["postCode"], "distance"=>$distance,"reviewScore"=>$reviewAverage);
 					$tempCount=$tempCount+1;
 				}//if the distance between the baker and customer is acceptable for both
-				//var_dump($JSONDistance);
 			}//while there is an item in the returned results
 			
 			return $validCompaniesArray;
 		}//end findValidShops
 	}//shopFinder class
-
+//ratings, distance from the shopper's postcode, and the services done by the shop
 
 
 
