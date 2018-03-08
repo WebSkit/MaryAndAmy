a5fa3628a624674b5a67eade5d6006d93e399a05
Ben123456789101112 <35489655+Ben123456789101112@users.noreply.github.com>
2018-03-08 13:48:27 +0000

    minor changes to bakerPage(bakerView).php
    -I have added a few buttons and changed some of the url links on some of the other buttons

--- bakerPage(bakersView).php
+++ bakerPage(bakersView).php
@@ -3,7 +3,7 @@ session_start();
 $requiredAddress="DAO/reviewsDAO.php";
 require($requiredAddress);
 $_SESSION["userId"]=1;
-$_SESSION["accountType"]="baker";//remove both of these tester session variables after you have completed this page
+$_SESSION["accountType"]="baker";
 
 if($_SESSION["accountType"]!="baker")
 {
@@ -12,11 +12,24 @@ if($_SESSION["accountType"]!="baker")
 else//else allow the rest of the page to be loaded
 {
 ?>
+
+<!--
+list of business types below
+1, Birthday cake
+  2, Christening / New baby cake
+  3, Anniversary cake
+  4, Wedding cake
+  5, Other celebration cakes
+- Cupcakes, Muffins
+- Muffin-cakes
+- Candy
+- Miscellaneous
+-->
 <!doctype html>
 	<head>
 
 	</head>
-
+	
 	<body>
 		<script type="text/javascript" src="bakerOptions/flagReview.js"></script>
 
@@ -26,7 +39,8 @@ else//else allow the rest of the page to be loaded
 			<article id="editDetails">
 				<a href="bakerOptions/changeDetails.php"><button type="button" name="editAccountDetails" value="Edit Account Details">Edit Account</button></a>
 				<a href="bakerOptions/changeServiceOptions.php"><button type="button"  name="serviceOptions" value="Change Serving Area or Delivery options">Change Service Options</button></a>
-				<a href="bakerOptions/uploadToGallery.php"><button type="button" name="uploadImages">Upload Images</button></a>
+				<a href="bakerOptions/changePassword.php"><button type="button" name="editPassword" value "Change Password">Change Password</button></a>
+				<a href="bakerOptions/uploadLogo.php"><button type="button" name="uploadImages">Upload Logo</button></a>
 			</article><!--end editDetails-->
 			<article id="orderControl">
 				<a href="bakerOptions/addProduct.php"><button type="button" name="addProduct">Create New Product</button></a>
@@ -59,11 +73,11 @@ else//else allow the rest of the page to be loaded
 												<li>By<?php echo " ".$reviewsArray[$tempCount]["customerName"];?></li>
 												<li><button class="<?php if($reviewsArray[$tempCount]["flagged"]==false){ echo "flagReview";}else{ echo "flagPending";}?>" type="button" data-reviewid="<?php echo $reviewsArray[$tempCount]["reviewId"]; ?>"><?php if($reviewsArray[$tempCount]["flagged"]==false){echo "Flag Review";}else{echo "Flag waiting Approval";}?></button></li>
 											</ul>
-										</li>
+										</li>	
 									<?php
 									$tempCount++;
 								}//while there are reviews in the list
-
+								
 							?>
 						</ul>
 						<?php
