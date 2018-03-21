79d8bbad35d39990e6b64d320e98a8f85fa9f5c3
jeffrey4530 <32640078+jeffrey4530@users.noreply.github.com>
2018-03-21 15:34:05 +0000

    Add files via upload

---
+++ classes/product.php
@@ -0,0 +1,44 @@
+<?php
+	class enquiry{
+		var $bakerId;
+		var $price;
+		var $productDescription;
+		
+		function __construct($bakerId, $price, $priceRange)
+		{
+			$this->setBakerId($bakerId);
+			$this->setPrice($price);
+			$this->setPriceRange($priceRange);
+		}//end of constructor
+
+		function setBakerId($bakerId)
+		{
+			$this->bakerId=$bakerId;
+		}
+		
+		function getBakerId()
+		{
+			return $this->bakerId;
+		}
+		
+		function setPrice($price)
+		{
+			$this->price=$price;
+		}
+		
+		function getPrice()
+		{
+			return $this->price;
+		}
+		
+		function setPriceRange($priceRange)
+		{
+			$this->priceRange=$priceRange;
+		}
+		
+		function getPriceRange()
+		{
+			return $this->priceRange;
+		}
+	}//end of product
+?>
